075641b7ed305b583f15e2dd055d3c2faa605d19
栗原遼太郎 <r_kurihara@my-macbook.local>
2022-12-14 20:51:10 +0900

    計算処理完成

--- app.py
+++ app.py
@@ -5,43 +5,22 @@ from typing import List, Dict, Union
 from dataclasses import dataclass, field
 import pdb 
 import re 
+import textwrap
+
 
 # ボットトークンと署名シークレットを使ってアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
-
-# 'hello' を含むメッセージをリッスンします
-@app.message("hello")
-def message_hello(message, say):
-    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text":"Click Me"},
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text=f"Hey there <@{message['user']}>!"
-    )
-
 token = os.environ["SLACK_BOT_TOKEN"]
-@app.message("uho")
-def test_client(client, message):
-    channel_id = message["channel"]
-    print(f"message:{message}")
-    thread_ts = message["thread_ts"]
-    print(f"thread_ts: {thread_ts}")
-    client.chat_postMessage(token=token, channel=channel_id, text="ウッヒョおおおおおお")
 
-    # スレッドメッセージの取得
+@app.message("計算")
+def test_client(client, message, say):
+    channel_id = message["channel"]
+    thread_ts = message["thread_ts"]
     thread_msgs = client.conversations_replies(token=token, channel=channel_id, ts=thread_ts)["messages"]
     pwt_list = list(_calculate_working_time_from_thread_msgs(thread_msgs))
-
-    
+    all_pwt = _calculate_all_person_work_time(pwt_list)
+    reply_msg = _create_message_from_pwt_object(all_pwt)
+    say(reply_msg, thread_ts=thread_ts)
 
 @dataclass
 class PersonWorkTime:
@@ -58,39 +37,49 @@ class PersonWorkTime:
         default_factory = lambda: {"pattern": "合計", "hour": 0}
     )
 
+    def _get_ins_var_keys(self) -> List[str]:
+        return list(self.__dict__.keys())
 
+    def _get_ins_var_values(self) -> List[Dict[str, Union[str, int]]]:
+        return list(self.__dict__.values())
+
+    def __add__(self, other):
+        values = self._get_ins_var_values()
+        other_values = other._get_ins_var_values()
+        for value, other_value in zip(values, other_values):
+            value["hour"] += other_value["hour"]
+        return self
+            
     def _find_match_pattern_and_add_work_time(self, message_text: str) -> None:
-        class_items = self.__dict__.items()  
-        for class_item in class_items:
-            # class_item: ('sales', {'pattern': '営業', 'hour': 0})みたいな中身になっている
-            calculate_class = class_item[1]["pattern"]
-            print(f"クラス：{calculate_class}")
+        values = self._get_ins_var_values()
+        for value in values:
+            calculate_class = value["pattern"]
             pattern = rf"[\s\S]*{calculate_class}\s([0-9]+)時間[\s\S]*"
             if m := re.match(pattern, message_text):
-                # TODO: ここの書き方！！！！
-                print(f"テキスト：{message_text}")
-                hour = m.groups()[0]
-                class_item[1]["hour"] += int(hour) 
-
-
-
+                hour = int(m.group(1))
+                value["hour"] += hour
 
 def _calculate_working_time_from_thread_msgs(thread_msgs: List[Dict]) -> PersonWorkTime:
     for msg in thread_msgs:
         pwt = PersonWorkTime()
         msg_txt = msg["text"]
-        print(f"thread_message解析中・・・: {msg_txt}")
         pwt._find_match_pattern_and_add_work_time(msg_txt)
         yield pwt 
 
+def _calculate_all_person_work_time(pwt_list: List[PersonWorkTime]) -> PersonWorkTime:
+    all_pwt = PersonWorkTime()
+    for pwt in pwt_list:
+        all_pwt += pwt
+    return all_pwt
 
+def _create_message_from_pwt_object(pwt: PersonWorkTime) -> str:
+    message = ""
+    values = pwt._get_ins_var_values()
+    for value in values:
+        message_line = f"{value['pattern']} {value['hour']}時間\n"
+        message += message_line
+    return message
 
-@app.action("button_click")
-def action_button_click(body, ack, say):
-    # アクションを確認したことを即時で応答します
-    ack()
-    # チャンネルにメッセージを投稿します
-    say(f"<@{body['user']['id']}> clicked the button")
 
 # アプリを起動します
 if __name__ == "__main__":
--- test_app.py
+++ test_app.py
@@ -1,4 +1,32 @@
 import unittest
+from unittest.mock import patch
+import textwrap
+
+from app import PersonWorkTime
+
 
 class TestSlackBot(unittest.TestCase):
-    def setup()
+    def setup(self):
+        self.bolt_app_m = patch("app.slack_bolt.App")
+        
+    def tearDown(self):
+        patch.stopall()
+
+    def test_find_match_pattern(self):
+        pwt = PersonWorkTime()
+        msg_txt = textwrap.dedent(
+            """\
+            事務作業 2時間
+            合計 3時間
+            """
+        )
+        pwt._find_match_pattern_and_add_work_time(msg_txt)
+        # 事務作業時間が正しく取得できているか
+        correct_office_hour = 2
+        self.assertEqual(correct_office_hour, pwt.office_work["hour"])
+        # 合計時間が正しく取得できているか
+        correct_total_hour = 3
+        self.assertEqual(correct_total_hour, pwt.total["hour"])
+
+if __name__ == "__main__":
+    unittest.main()
